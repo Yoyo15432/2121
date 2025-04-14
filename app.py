892b665e3b4ff7ef968b8476c1905f2ca05d3bf9
Yoyo15432 <the.yomans.club@gmail.com>
2025-04-15 00:52:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -458,7 +458,7 @@ def contacter_client(id):
     <html> 
     <head></head>
     <body> 
-    <form action='/contacter/""" + id +"""/"""+cle_personne+"""' method="post">
+    <form action='https://two121-1.onrender.com/contacter/""" + id +"""/"""+cle_personne+"""' method="post">
     <p>Bonjour,<br> l'agent immobilier """+ nom_agence +""" aimerais vous contacter à propos de la recherche que vous avez effectuer suivante.<br>
     Pour lui transmettre vos coordonnées (téléphone, nom et prénom) via le site, <input type="submit" value="cliquez ici">. 
     </p>
